f9fc11617b8edb2428e44e15597826222ff403ee
Uppitydonkey57 <51006437+Uppitydonkey57@users.noreply.github.com>
2021-04-25 08:47:14 -0400

    made it not framerate dependant

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -35,16 +35,26 @@ public class PlayerMovement : MonoBehaviour
     [Range(0, 1)]
     float fCutJumpHeight = 0.5f;
 
-    void Start ()
+    float horizontal;
+    bool jump;
+    bool jumpUp;
+
+    void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-	}
-	
-	void Update ()
+    }
+
+    private void Update()
     {
         Vector2 v2GroundedBoxCheckPosition = (Vector2)transform.position + new Vector2(0, -0.01f);
         Vector2 v2GroundedBoxCheckScale = (Vector2)transform.localScale + new Vector2(-0.02f, 0);
         bool bGrounded = Physics2D.OverlapBox(v2GroundedBoxCheckPosition, v2GroundedBoxCheckScale, 0, lmWalls);
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        jump = Input.GetButtonDown("Jump");
+
+        jumpUp = Input.GetButtonUp("Jump");
 
         fGroundedRemember -= Time.deltaTime;
         if (bGrounded)
@@ -53,18 +63,22 @@ public class PlayerMovement : MonoBehaviour
         }
 
         fJumpPressedRemember -= Time.deltaTime;
-        if (Input.GetButtonDown("Jump"))
+        if (jump)
         {
             fJumpPressedRemember = fJumpPressedRememberTime;
         }
 
-        if (Input.GetButtonUp("Jump"))
+        if (jumpUp)
         {
             if (rigid.velocity.y > 0)
             {
                 rigid.velocity = new Vector2(rigid.velocity.x, rigid.velocity.y * fCutJumpHeight);
             }
         }
+    }
+
+    void FixedUpdate()
+    {
 
         if ((fJumpPressedRemember > 0) && (fGroundedRemember > 0))
         {
@@ -74,15 +88,15 @@ public class PlayerMovement : MonoBehaviour
         }
 
         float fHorizontalVelocity = rigid.velocity.x;
-        fHorizontalVelocity += Input.GetAxisRaw("Horizontal");
+        fHorizontalVelocity += horizontal;
 
-        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) < 0.01f)
+        if (Mathf.Abs(horizontal) < 0.01f)
             fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenStopping, Time.deltaTime * 10f);
-        else if (Mathf.Sign(Input.GetAxisRaw("Horizontal")) != Mathf.Sign(fHorizontalVelocity))
-            fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenTurning, Time.deltaTime * 10f);
+        else if (Mathf.Sign(horizontal) != Mathf.Sign(fHorizontalVelocity))
+            fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingWhenTurning, Time.fixedDeltaTime * 10f);
         else
-            fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingBasic, Time.deltaTime * 10f);
+            fHorizontalVelocity *= Mathf.Pow(1f - fHorizontalDampingBasic, Time.fixedDeltaTime * 10f);
 
-        rigid.velocity = new Vector2(fHorizontalVelocity, rigid.velocity.y);
+        rigid.velocity = new Vector2(fHorizontalVelocity * fHorizontalAcceleration, rigid.velocity.y);
     }
 }
